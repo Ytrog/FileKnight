3cbe5c11cf74a98de24cf4163e8969fd8ede232b
Ytrog <hisbrucker@gmail.com>
2015-02-24 23:48:14 +0100

    testing commanline parsing

---
+++ FileKnight/FileKnight/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileKnight
+{
+    class Options
+    {
+        [Option('f', "file", Required=true, HelpText="File to hash")]
+        public string File { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = CommandLine.Text.HelpText.AutoBuild(this);
+            return help;
+        }
+    }
+}
--- FileKnight/FileKnight/Program.cs
+++ FileKnight/FileKnight/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using FileKnight.Core;
+using System.IO;
 
 namespace FileKnight
 {
@@ -13,11 +14,26 @@ namespace FileKnight
         static void Main(string[] args)
         {
 
-            
+            var parser = CommandLine.Parser.Default;
+            Options options = new Options();
+            if(parser.ParseArguments(args, options))
+            {
+                HashFile(options.File);
+            }
 
 #if DEBUG
             Console.ReadKey();
 #endif
+        }
+
+        private static void HashFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                var input = File.ReadAllBytes(path);
+                var output = Hasher.HashToString(input);
+                Console.WriteLine(output);
+            }
         }
 
         
